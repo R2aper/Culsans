bad86858c499d9c663561cf0e87099f702b5f5fb
Mihail <MihailZharikow@yandex.ru>
2026-04-20 19:09:38 +0700

    feat(main): Implement simple encryption/decryption in main function

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/R2aper/Culsans
 
 go 1.25.4
+
+require (
+	github.com/ProtonMail/gopenpgp/v3 v3.4.0
+	golang.org/x/term v0.34.0
+)
+
+require (
+	github.com/ProtonMail/go-crypto v1.4.1 // indirect
+	github.com/cloudflare/circl v1.6.2 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,7 +4,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+
+	"golang.org/x/term"
 )
 
 func usage() {
@@ -12,10 +16,10 @@ func usage() {
 		"Usage: cl [Options] <command> \n" +
 		"Password manager\n" +
 		"\nOptions:" +
-		"-h,--help\tShow this help message\n" +
-		"-v,--version\t\tShow version\n" +
-		"-q,--quiet\t\tDon't commit changes\n" +
-		"-m,--commit-message\tSpecify commit message\n" +
+		"-h\tShow this help message\n" +
+		"-v\t\tShow version\n" +
+		"-q\t\tDon't commit changes\n" +
+		"-m\tSpecify commit message\n" +
 		"\nCommands:\n" +
 		"init\t\t\tInitialize a new git repository in the current working directory(Similar to git init)\n" +
 		"list\t\t\tList all passwords in the vault\n" +
@@ -25,5 +29,92 @@ func usage() {
 }
 
 func main() {
-	usage()
+	// Flags
+	help_flag := flag.Bool("h", false, "Print usage")
+	version_flag := flag.Bool("v", false, "Print version")
+
+	// States
+	key_value := flag.String("k", "", "Specify public/private key path")
+
+	// Modes
+	add_flag := flag.String("add", "", "Specify output file name")
+	remove_flag := flag.String("remove", "", "TODO")
+	show_flag := flag.String("show", "", "Specify input file name")
+
+	flag.Parse()
+
+	if *help_flag {
+		usage()
+		return
+	}
+
+	if *version_flag {
+		fmt.Println("0.0")
+		return
+	}
+
+	if *add_flag != "" {
+		fmt.Println("Enter message:")
+		msg, err := term.ReadPassword(int(os.Stdin.Fd()))
+		defer func() {
+			for i := range msg {
+				msg[i] = 0
+			}
+		}()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "\nError while reading: %v\n", err)
+			return
+		}
+		fmt.Println()
+
+		ciphertext, err := encryptWithPublicKey(*key_value, msg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			return
+		}
+
+		os.WriteFile(*add_flag, ciphertext, 0644)
+		// TODO: commit + sign
+		return
+	}
+
+	if *remove_flag != "" {
+		fmt.Println("TODO")
+	}
+
+	if *show_flag != "" {
+		fmt.Println("Enter passphrase:")
+		pass, err := term.ReadPassword(int(os.Stdin.Fd()))
+		defer func() {
+			for i := range pass {
+				pass[i] = 0
+			}
+		}()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "\nError while reading: %v\n", err)
+			return
+		}
+		fmt.Println()
+
+		msg, err := os.ReadFile(*show_flag)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			return
+		}
+
+		data, err := decryptWithPrivateKey(*key_value, pass, msg)
+		defer func() {
+			for i := range data {
+				data[i] = 0
+			}
+		}()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			return
+		}
+
+		fmt.Printf("Data:\n%s\n", string(data))
+		return
+	}
+
 }
